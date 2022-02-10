3d20af876d1e95b7f3932d2d5b4360ee4e5f9a9d
ksertas <k.serkanertas@gmail.com>
2022-02-10 19:35:13 +0100

    create custom MUI theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { createMuiTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from './components/Navbar';
+import OptionsBar from './components/OptionsBar';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Source Sans Pro',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
 
 export default function App() {
   return (
-    <div className='h-screen bg-white dark:bg-black'>
-      <Navbar />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='h-screen bg-white dark:bg-black'>
+        <Navbar />
+        <OptionsBar />
+      </div>
+    </ThemeProvider>
   );
 }
