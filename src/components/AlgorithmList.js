da6f89898a0d208e2ef9c2b2854fe4bc66a1ca31
ksertas <k.serkanertas@gmail.com>
2022-02-11 19:00:36 +0100

    rename algo state to currentAlgo

--- src/components/AlgorithmList.js
+++ src/components/AlgorithmList.js
@@ -5,12 +5,12 @@ const algorithms = ['bubble sort', 'quick sort', 'merge sort', 'selection sort']
 
 
 export default function AlgorithmList() {
-  const [algo, setAlgo] = useState('bubble sort');
+  const [currentAlgo, setCurrentAlgo] = useState('bubble sort');
 
   return (
     <ul className='text-white text-xl'>
       {algorithms.map((algorithm, i) => {
-        return <li key={i} onClick={()=> setAlgo(algorithm)} className='py-2 flex items-center group cursor-pointer focus:bg-white'><Button className='w-full' {...(algorithm === algo ? {variant: 'contained'} : {variant: 'outlined'})}>{algorithm}</Button></li>;
+        return <li key={i} onClick={()=> setCurrentAlgo(algorithm)} className='py-2 flex items-center group cursor-pointer focus:bg-white'><Button className='w-full' {...(algorithm === currentAlgo ? {variant: 'contained'} : {variant: 'outlined'})}>{algorithm}</Button></li>;
       })}
     </ul>
   );
