ee7612a1eb0dd5a1451b545b8847272d680a56ff
ksertas <k.serkanertas@gmail.com>
2022-02-28 14:19:35 +0100

    add raw selection sort

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -12,3 +12,19 @@ function bubbleSort(arr) {
   return arr;
 }
 
+function selectionSort(arr) {         
+  for(let i = 0; i < arr.length; i++) {
+    let min = i;
+    for(let j = i+1; j < arr.length; j++){
+      if(arr[j] < arr[min]) {
+        min=j; 
+      }
+    }
+    if (min != i) {
+      let tmp = arr[i]; 
+      arr[i] = arr[min];
+      arr[min] = tmp;      
+    }
+  }
+  return arr;
+}
