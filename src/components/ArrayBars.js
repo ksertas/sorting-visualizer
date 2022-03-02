a95d3ad5c7eb40a86d826d4ba69c818cbb56300d
ksertas <k.serkanertas@gmail.com>
2022-03-02 17:58:23 +0100

    refactor generated width and height into variables

--- src/components/ArrayBars.js
+++ src/components/ArrayBars.js
@@ -2,25 +2,25 @@ import React, { useContext } from 'react';
 import { SortingSettingsContext } from '../contexts/SortingSettingsContext';
 import { getRandomInt } from '../helper/randomInt';
 
-
 export default function ArrayBars() {
   const {arrayLength, invertBars} = useContext(SortingSettingsContext);
   
   let generatedDivs = [];
   for (let i = 1; i < arrayLength + 1; i++) {
+    const generatedWidth = parseFloat(100 / arrayLength);
+    const generatedHeight = getRandomInt(1, 100);
     
     let style = {
       position: 'absolute',
-      width: `${parseFloat(100 / arrayLength)}%`,
-      height: `${getRandomInt(1, 100)}%`,
+      width: `${generatedWidth}%`,
+      height: `${generatedHeight}%`, 
       left: `${100 / arrayLength * parseFloat(i - 1)}%`,
       bottom: 0
     };
   
-    let heightWithoutPercentage = style.height.split('%')[0];
-    let showHeight = (arrayLength <= 20) && (heightWithoutPercentage > 10);
-  
-    generatedDivs.push(<div key={i} style={style} className={`bg-red-400 border-[1px] border-red-500 absolute block text-white text-center ${showHeight ? 'text-[.75rem]' : 'text-[0px]'}`}>{heightWithoutPercentage}</div>);
+    let showHeight = (arrayLength <= 20) && (generatedHeight > 10);
+
+    generatedDivs.push(<div key={i} style={style} className={`bar bg-red-400 border-[1px] border-red-500 absolute block text-white text-center ${showHeight ? 'text-[.75rem]' : 'text-[0px]'}`}>{generatedHeight}</div>);
   }
 
   return (
