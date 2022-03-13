93ec09e17dce279bcc34f0d7018d8f4cfdd28f1a
ksertas <k.serkanertas@gmail.com>
2022-03-13 20:08:53 +0100

    Temporarily add window reload for reset button
    This is until proper stop/reset features have been implemented

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,11 +4,12 @@ import { AlgorithmContext } from '../contexts/AlgorithmContext';
 
 export default function Controls() {
 
-  const {setRunning} = useContext(AlgorithmContext);
+  const {isRunning, setRunning} = useContext(AlgorithmContext);
 
   return (
     <div className='bg-container-dark w-24 mx-auto flex justify-center py-4 px-16 mt-48 lg:mt-0 lg:mx-0 lg:col-start-3 lg:col-span-1 lg:w-auto xl:col-start-3 xl:col-span-2'>
-      <Button variant='contained' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => setRunning(true)}>Run</Button>
+      { !isRunning ? <Button variant='contained' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => setRunning(true)}>Run</Button> :
+        <Button variant='contained' color='error' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => window.location.reload()}>Reset</Button>}
     </div>
   );
 }
