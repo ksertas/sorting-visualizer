0f8157ac8c8f0ad40d823cb3c76fc87b8f6b1df6
ksertas <k.serkanertas@gmail.com>
2022-03-13 20:04:36 +0100

    Refactor div array generation into function

--- src/components/ArrayBars.js
+++ src/components/ArrayBars.js
@@ -4,25 +4,29 @@ import { getRandomInt } from '../helper/randomInt';
 
 export default function ArrayBars() {
   const {arrayLength, invertBars} = useContext(SortingSettingsContext);
-  
   let generatedDivs = [];
-  for (let i = 1; i < arrayLength + 1; i++) {
-    const generatedWidth = parseFloat(100 / arrayLength);
-    const generatedHeight = getRandomInt(5, 100);
-    
-    let style = {
-      position: 'absolute',
-      width: `${generatedWidth}%`,
-      height: `${generatedHeight}%`, 
-      left: `${100 / arrayLength * parseFloat(i - 1)}%`,
-      bottom: 0,
-      backgroundColor: '#4295d6'
-    };
   
-    let showHeight = (arrayLength <= 20) && (generatedHeight > 10);
+  const generateNewArray = () => {
+    for (let i = 1; i < arrayLength + 1; i++) {
+      const generatedWidth = parseFloat(100 / arrayLength);
+      const generatedHeight = getRandomInt(5, 100);
+      
+      let style = {
+        position: 'absolute',
+        width: `${generatedWidth}%`,
+        height: `${generatedHeight}%`, 
+        left: `${100 / arrayLength * parseFloat(i - 1)}%`,
+        bottom: 0,
+        backgroundColor: '#4295d6'
+      };
+    
+      let showHeight = (arrayLength <= 20) && (generatedHeight > 10);
+  
+      generatedDivs.push(<div key={i} style={style} className={`bar border-[1px] border-black absolute block text-white text-center ${showHeight ? 'text-[.75rem]' : 'text-[0px]'}`}>{generatedHeight}</div>);
+    }
+  };
 
-    generatedDivs.push(<div key={i} style={style} className={`bar border-[1px] border-black absolute block text-white text-center ${showHeight ? 'text-[.75rem]' : 'text-[0px]'}`}>{generatedHeight}</div>);
-  }
+  generateNewArray();
 
   return (
     <div className={`bg-container-dark ${invertBars ? 'rotate-180' : ''} w-10/12 h-[15rem] mx-auto mt-6 block relative lg:my-auto lg:col-span-3 xl:col-span-4`}>
