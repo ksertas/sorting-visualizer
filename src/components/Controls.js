f01793f02382ba4adf9dce33f5ec70996813d6f3
ksertas <k.serkanertas@gmail.com>
2022-03-14 21:14:51 +0100

    Add stop function to replace window reload

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -6,13 +6,13 @@ import { SortingSettingsContext } from '../contexts/SortingSettingsContext';
 export default function Controls() {
 
   const {arrayLength, setArrayLength} = useContext(SortingSettingsContext);
-  const {isRunning, setRunning, isFinished, setFinished} = useContext(AlgorithmContext);
+  const {isRunning, setRunning, isFinished} = useContext(AlgorithmContext);
 
   if (arrayLength !== 0) {
     localStorage.setItem('speed', arrayLength);
   }
   
-  const handleClick = () => {
+  const runAgain = () => {
     setArrayLength(0);
     setTimeout(() => {
       setArrayLength(parseInt(localStorage.getItem('speed')));
@@ -20,13 +20,21 @@ export default function Controls() {
     }, 0);
   };
 
+  const stopSort = () => {
+    setArrayLength(0);
+    setTimeout(() => {
+      setArrayLength(parseInt(localStorage.getItem('speed')));
+      setRunning(false);
+    }, 0);
+  };
+
   return (
     <div className='bg-white-100 dark:bg-container-dark w-24 rounded-sm mx-auto flex justify-center py-4 px-16 mt-24 lg:mt-0 lg:mx-0 lg:col-start-3 lg:col-span-1 lg:w-auto xl:col-start-3 xl:col-span-2'>
       { !isRunning && isFinished ? 
-        <Button variant='contained' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => handleClick()}>Run again</Button> :
+        <Button variant='contained' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => runAgain()}>Run again</Button> :
         !isRunning ?
           <Button variant='contained' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => setRunning(true)}>Run</Button> :
-          <Button variant='contained' color='error' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => window.location.reload()}>Stop</Button>}
+          <Button variant='contained' color='error' className='lg:w-24 lg:h-12 xl:w-32' onClick={() => stopSort()}>Stop</Button>}
     </div>
   );
 }
