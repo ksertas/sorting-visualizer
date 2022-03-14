c3394f6628347ea97eee60b506d77b419a0cbd13
ksertas <k.serkanertas@gmail.com>
2022-03-14 13:02:05 +0100

    Add check to see if array is sorted in intervals

--- src/contexts/AlgorithmContext.js
+++ src/contexts/AlgorithmContext.js
@@ -9,31 +9,46 @@ export default function AlgorithmProvider({children}) {
   const [isRunning, setRunning] = useState(false);
 
   const runSelectedAlgorithm = () => {
-    if (isRunning) {
-      switch (selectedAlgorithm) {
-      case 'Bubble sort':
-        bubbleSort();
-        break;
-      case 'Quicksort':
-        runQuickSort();
-        break;
-      case 'Insertion sort':
-        insertionSort();
-        break;
-      case 'Selection sort':
-        selectionSort();
-        break;
+    switch (selectedAlgorithm) {
+    case 'Bubble sort':
+      bubbleSort();
+      break;
+    case 'Quicksort':
+      runQuickSort();
+      break;
+    case 'Insertion sort':
+      insertionSort();
+      break;
+    case 'Selection sort':
+      selectionSort();
+      break;
     
-      default:
-        break;
-      }
+    default:
+      break;
     }
   };
 
   useEffect(() => {
-    runSelectedAlgorithm();
+    if (isRunning) {
+      runSelectedAlgorithm();
+      checkIfSortIsDone();
+    }
   }, [isRunning]);
   
+  const checkIfSortIsDone = () => {
+    setInterval(() => {
+      let divs = document.querySelectorAll('.bar');
+      let isSorted = 0;
+      for (let i = 0; i < divs.length; i++) {
+        if (divs[i].classList.contains('sorted')) {
+          isSorted++;
+        }
+      }
+      if (isSorted === divs.length) {
+        setRunning(false);
+      }
+    }, 1000);
+  };
 
   const contextData = {
     selectedAlgorithm: selectedAlgorithm,
